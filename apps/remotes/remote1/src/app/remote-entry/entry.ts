730b2b7b500fa7b2094ced1f4d56861fd711c3c3
ahmed <nesho.a.ali@gmail.com>
2025-10-03 22:08:15 +0300

    mf working

--- apps/remotes/remote1/src/app/remote-entry/entry.ts
+++ apps/remotes/remote1/src/app/remote-entry/entry.ts
@@ -1,16 +1,15 @@
 import { Component } from '@angular/core';
-import { NxWelcome } from './nx-welcome';
 import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
-  imports: [NxWelcome, RouterLink, RouterOutlet],
+  imports: [RouterLink, RouterOutlet],
   selector: 'nts-remote1-entry',
   template: `
     <ul>
       <li><a routerLink="login">Login</a></li>
       <li><a routerLink="welcome">Home</a></li>
     </ul>
-<router-outlet/>
+    <router-outlet />
   `,
 })
 export class RemoteEntry {}
--- apps/remotes/remote1/webpack.prod.config.ts
+++ apps/remotes/remote1/webpack.prod.config.ts
@@ -20,5 +20,5 @@ export default withModuleFederation(
      * ]
      */
   },
-  { dts: false }
+  { dts: false },
 );
